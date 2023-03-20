ac74bd33cfe275c8e63893aa515f715a55ce851d
kvitorr <kvitorrsantos@gmail.com>
2023-03-19 23:36:11 -0300

    Event Handling

--- estados_de_componente_e_gerenciador_de_eventos/estados-e-eventos/src/App.tsx
+++ estados_de_componente_e_gerenciador_de_eventos/estados-e-eventos/src/App.tsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
-function App() {
-  
+import { MouseEventHandler, useState } from "react"
+
+interface Contador {
+  contador: number
+}
+
+interface Butto {
+  texto: string
+  onClick: Function
+}
+
+const Exibir: React.FC<Contador> = ({contador}) => <div>{contador}</div>
+
+const Button: React.FC<Butto> = ({texto, onClick}) => 
+    <button onClick={onClick as MouseEventHandler<HTMLButtonElement>}>
+      {texto}
+    </button>
+
+
+const App = () => {
+  const [ contador, setContador ] = useState(0)
+  const aumentarEmUm = () => setContador(contador + 1)
+  const diminuirEmUm = () => setContador(contador - 1)
+  const zerarContador = () => setContador(0)
+
   return (
-    <></>
+    <div>
+      <Exibir contador={contador}/>
+      <Button 
+        onClick={aumentarEmUm} 
+        texto={'mais 1'}
+      />
+      <Button 
+        onClick={zerarContador} 
+        texto={'zerar'}
+      />
+      <Button 
+        onClick={diminuirEmUm} 
+        texto={'menos 1'}
+      />
+    </div>
   )
 }
+
 
 export default App
