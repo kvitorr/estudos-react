f47de120b6ac5a53b066ae2ee626484669d33632
kvitorr <kvitorrsantos@gmail.com>
2023-03-22 07:18:06 -0300

    Create page of statistics

---
+++ estados_de_componente_e_gerenciador_de_eventos/unicafe/src/App.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+interface Butto {
+  texto: string
+  onClick: any
+}
+
+interface IStatistics {
+  good: number
+  bad: number
+  neutral: number
+  votes: number
+  average: number
+  positive: number
+}
+
+interface IStatisticsLine {
+  texto: string
+  valor: number
+}
+
+const StatisticLine: React.FC<IStatisticsLine> = ({texto, valor}) => {
+
+  return (
+    <>
+      {texto} {valor}
+    </>
+  )
+
+}
+
+const Button: React.FC<Butto> = ({texto, onClick}) => {
+  return (
+    <button onClick={onClick}>
+      {texto}
+    </button>
+  )
+}
+
+const Statistics: React.FC<IStatistics> = ({good, neutral, bad, votes, average, positive}) => {
+
+  if(votes === 0) {
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p> 
+      </div>
+    )  
+  }
+  return(
+    <div>
+      <StatisticLine texto='good' valor={good}/><br/>
+      <StatisticLine texto='neutral' valor={neutral}/><br/>
+      <StatisticLine texto='bad' valor={bad}/><br/>
+      <StatisticLine texto='all' valor={votes}/><br/>
+      <StatisticLine texto='average' valor={average}/><br/>
+      <StatisticLine texto='positive' valor={positive}/><br/>
+    </div>
+  )
+}
+
+function App() {
+  const [statistics, setStatistics] = useState({
+    good: 0, bad: 0, neutral: 0, votes: 0, average: 0, positive: 0
+  })
+
+  const setGoodF = () => {
+    const updatedGood = statistics.good + 1
+    const updatedVotes = statistics.votes + 1
+    const updatedAverage = statistics.average + 1 
+    const updatedPositive = (updatedGood)/updatedVotes * 100
+
+    const updatedStatistics = {
+      ...statistics,
+      good: updatedGood,
+      votes: updatedVotes,
+      average: updatedAverage,
+      positive: updatedPositive
+    }
+
+    setStatistics(updatedStatistics)
+  }
+
+  const setBadF = () => {
+    const updatedBad = statistics.bad + 1
+    const updatedVotes = statistics.votes + 1
+    const updatedAverage = statistics.average - 1 
+    const updatedPositive = (statistics.good)/updatedVotes * 100
+
+    const updatedStatistics = {
+      ...statistics,
+      bad: updatedBad,
+      votes: updatedVotes,
+      average: updatedAverage,
+      positive: updatedPositive
+    }
+
+    setStatistics(updatedStatistics)
+  }
+
+  const setNeutralF = () => {
+    const updatedNeutral = statistics.neutral + 1
+    const updatedVotes = statistics.votes + 1
+    const updatedPositive = (statistics.good)/updatedVotes * 100
+
+    const updatedStatistics = {
+      ...statistics,
+      neutral: updatedNeutral,
+      votes: updatedVotes,
+      positive: updatedPositive
+    }
+
+    setStatistics(updatedStatistics)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={setGoodF} texto={'good'}/>
+      <Button onClick={setNeutralF} texto={'neutral'}/>
+      <Button onClick={setBadF} texto={'bad'}/>
+      
+      <h1>statistics</h1>
+      <Statistics {...statistics}/>
+    </div>
+  )
+}
+
+export default App
