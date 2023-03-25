4e5332a371ced6f7e1763de2a38056f880609fac
kvitorr <kvitorrsantos@gmail.com>
2023-03-25 06:43:01 -0300

    Note: Add a new Note and Show Notes options

--- rendering-collections/src/App.tsx
+++ rendering-collections/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Note from "./components/Note"
 
 interface INotes {
@@ -10,14 +11,49 @@ interface INotes {
 
 
 const App: React.FC<INotes> = ({notes}) => {
+  
+  const [currentNotes, setNotes] = useState(notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event: any) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: currentNotes.length + 1 
+    }
+
+    setNotes(currentNotes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event: any) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? currentNotes
+    : currentNotes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-          {notes.map((note: any) => 
+          {notesToShow.map((note: any) => 
             <Note key={note.id} {...note}/>
           )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
