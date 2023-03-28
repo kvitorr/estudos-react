3a2889f0707afbf9d1838e8f162948b1c88433fb
kvitorr <kvitorrsantos@gmail.com>
2023-03-27 22:30:01 -0300

    Add Function: Delete Person

--- phonebook/src/components/Persons.tsx
+++ phonebook/src/components/Persons.tsx
@@ -1,16 +1,38 @@
 import { Person } from "../interfaces/Person"
+import phonebookService from "../services/phonebookService"
 
 type Props = {
     persons: Person[],
-    searchTerm: string
+    searchTerm: string,
+    setPersons: any
 }
 
-const Persons: React.FC<Props> = ({persons, searchTerm}) => {
+const Persons: React.FC<Props> = ({persons, searchTerm, setPersons}) => {
     const personsFiltered = persons.filter((person) => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    
+
+    const deletePerson = (id: number, name: string) => {
+
+        if(window.confirm(`Delete ${name}?`)) {
+            phonebookService
+            .remove(id)
+            .then( () => {
+              const filteredPersons = persons.filter((person: Person) => person.id != id)
+              setPersons(filteredPersons)
+            })
+        }
+    }
+
     return (
         <>
             {personsFiltered.map((person: any) => {
-                return <li key={person.id}>{person.name} {person.number}</li>
+                return (
+                    <div key={person.id}>
+                        {person.name} {person.number} 
+                        <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
+                    </div>
+                )
             })}
         </>
     )
